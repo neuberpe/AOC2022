fbb81b2d68761d27b4591f36cc6db8bd1c670ad3
neuberpe <36083955+neuberpe@users.noreply.github.com>
2022-12-04 11:13:27 +0100

    Update AOC4_2022.py

--- AOC4_2022.py
+++ AOC4_2022.py
@@ -12,7 +12,7 @@ z2=0
 while i < len(mylist):	#zuerst werden ganze listen aus den angaben gemacht mit range und string checks
 	a = list(range(int(str(mylist[i])[:mylist[i].find("-")]),int(str(mylist[i])[mylist[i].find("-")+1:])+1))
 	b = list(range(int(str(mylist[i+1])[:mylist[i+1].find("-")]),int(str(mylist[i+1])[mylist[i+1].find("-")+1:])+1))
-	if set(a).issubset(b) or set(b).issubset(a):	#ueberprueft, ob eine liste in einer anderen liste vollständig enthalten ist
+	if set(a).issubset(b) or set(b).issubset(a):	#ueberprueft, ob eine liste in einer anderen liste vollstaendig enthalten ist
 		z+=1
 	if bool(set(a) & set(b)):		#ueberprueft, ob es ueberlappende elemente gibt
 		z2+=1
